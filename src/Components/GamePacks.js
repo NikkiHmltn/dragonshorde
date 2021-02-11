48a26ef0e806dfdd9f1437f543428911c0a41628
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-10 21:54:33 -0800

    collections has text and styling along with fadein animation

--- src/Components/GamePacks.js
+++ src/Components/GamePacks.js
@@ -9,17 +9,29 @@ function GamePacks() {
     return(
         <>
         <NavBar />
+        <h2 className="col-title">What We Carry</h2>
         <div className="collection-container">
-            <div className="yugioh">
+            <div className="yugioh fade-in">
                 <img src={yugioh} />
+
             </div>
-            <div className="magic">
+            <div className="magic fade-in">
                 <img src={magic} />
+               
             </div>
-            <div className="pokemon">
+            <div className="pokemon fade-in">
                 <img src={poke}/>
+              
             </div>
         </div>
+        <div className="information">
+            <p>Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. At tempor commodo ullamcorper a lacus vestibulum sed. Dui faucibus in ornare quam viverra orci sagittis eu. Tristique nulla aliquet enim tortor at auctor. Ipsum consequat nisl vel pretium lectus quam id leo. Turpis cursus in hac habitasse platea. Elementum nisi quis eleifend quam adipiscing vitae proin.</p> 
+            <br></br>
+            <p>Condimentum vitae sapien pellentesque habitant morbi. Sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum. Sit amet nisl purus in mollis nunc. Convallis tellus id interdum velit laoreet id donec. Tincidunt vitae semper quis lectus nulla at volutpat. Velit euismod in pellentesque massa placerat duis ultricies lacus. Id venenatis a condimentum vitae sapien. Turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet. Amet risus nullam eget felis eget nunc lobortis mattis.</p>
+            <br></br>
+            <p>Have questions or just want to talk? <a href="tel:+1">Call us</a> or <a href="/contact">send us a message</a>!</p>
+        </div>
+
         <Footer />
         </>
     )
