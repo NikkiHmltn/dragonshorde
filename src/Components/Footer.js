f0e7df1034251bae5db0aef929a9232b31e1864e
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-10 20:26:19 -0800

    navbar and footer add styling and links for facebook and yelp

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,8 @@ import './css/Footer.css'
 function Footer() {
     return(
         <div className="footer">
+            <a href="#"><img src="https://img.icons8.com/dotty/80/000000/facebook-new.png"/></a>
+            <a href="#"><img src="https://img.icons8.com/wired/64/000000/yelp.png"/></a>
             <p className="foot-note">© The Dragon's Horde | Address Here | Phone Here</p>
         </div>
     )
