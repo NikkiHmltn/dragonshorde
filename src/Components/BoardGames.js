af737f93c257037a81ee8feaeebb0861904787e5
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-13 21:22:00 -0800

    set up cards for boardgames

--- src/Components/BoardGames.js
+++ src/Components/BoardGames.js
@@ -1,7 +1,13 @@
 import NavBar from './NavBar'
 import Footer from './Footer'
 import './css/Boardgames.css'
-import boardgames from './imgs/boardgames.jpg'
+import catangame from './imgs/catangame.jpg'
+import munchkin from './imgs/munchkin.jpg'
+import betrayl from './imgs/betrayl.jpg'
+import lords from './imgs/lordsofwaterdeep.jpg'
+import axisally from './imgs/axisallies.jpg'
+import horizon from './imgs/horizon.jpg'
+
 
 function BoardGames() {
     return(
@@ -23,10 +29,48 @@ function BoardGames() {
         </section>
         
         <div className="boardgame-items">
-        <p>Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. At tempor commodo ullamcorper a lacus vestibulum sed. Dui faucibus in ornare quam viverra orci sagittis eu. Tristique nulla aliquet enim tortor at auctor. Ipsum consequat nisl vel pretium lectus quam id leo. Turpis cursus in hac habitasse platea. Elementum nisi quis eleifend quam adipiscing vitae proin.</p> 
-        <br>
-        </br>
-        <p>Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. At tempor commodo ullamcorper a lacus vestibulum sed. Dui faucibus in ornare quam viverra orci sagittis eu. Tristique nulla aliquet enim tortor at auctor. Ipsum consequat nisl vel pretium lectus quam id leo. Turpis cursus in hac habitasse platea. Elementum nisi quis eleifend quam adipiscing vitae proin.</p> 
+            <div class="card" style={{width: "18rem"}}>
+                <img class="card-img-top" height="70%" width="90%" src={catangame} alt="Catan" />
+                <div class="card-body">
+                    <h5 class="card-title">Catan</h5>
+                    <p class="card-text">....</p>
+                </div>
+            </div>
+            <div class="card" style={{width: "18rem"}}>
+                <img class="card-img-top" src={munchkin} alt="Munchkin" />
+                <div class="card-body">
+                    <h5 class="card-title">Munchkin</h5>
+                    <p class="card-text">....</p>
+                </div>
+            </div>
+            <div class="card" style={{width: "18rem"}}>
+                <img class="card-img-top" src={betrayl} alt="Betrayl at House on the Hill" />
+                <div class="card-body">
+                    <h5 class="card-title">Betrayl at House on the Hill</h5>
+                    <p class="card-text">....</p>
+                </div>
+            </div>
+            <div class="card" style={{width: "18rem"}}>
+                <img class="card-img-top" src={lords} alt="Lords of Waterdeep" />
+                <div class="card-body">
+                    <h5 class="card-title">Lords of Waterdeep</h5>
+                    <p class="card-text">....</p>
+                </div>
+            </div>
+            <div class="card" style={{width: "18rem"}}>
+                <img class="card-img-top" src={axisally} alt="Axis/Allies" />
+                <div class="card-body">
+                    <h5 class="card-title">Axis/Allies</h5>
+                    <p class="card-text">....</p>
+                </div>
+            </div>
+            <div class="card" style={{width: "18rem"}}>
+                <img class="card-img-top" src={horizon} alt="Horizon Zero Dawn" />
+                <div class="card-body">
+                    <h5 class="card-title">Horizon Zero Dawn</h5>
+                    <p class="card-text">....</p>
+                </div>
+            </div>
         </div>
         <div className="paralax"></div>
         <Footer />
