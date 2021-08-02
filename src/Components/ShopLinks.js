21de66d0a3a92e7872c50199fb4f8284f03075fb
nicole <nikkihmltn1@yahoo.com>
2021-08-02 01:08:33 -0700

    shoplinks responsive

--- src/Components/ShopLinks.js
+++ src/Components/ShopLinks.js
@@ -1,6 +1,7 @@
 import magicbox from './imgs/magic-box-shop.png'
 import supplies from './imgs/supplies.jpeg'
 import boardgame from './imgs/DnD_LOW.png'
+import {Link} from 'react-router-dom'
 import './css/ShopLinks.css'
 
 function ShopLinks() {
@@ -8,18 +9,16 @@ function ShopLinks() {
         <div className="shop-items">
             <div className="links-shop">
                 <div className="shop-titles">
-                    <h2>CARDS</h2>
-                    <h2>SUPPLIES</h2>
-                    <h2>BOARDGAMES</h2>
+                <Link to="/collections"><h2>CARDS</h2></Link>
+                <Link to="/collections"><h2>SUPPLIES</h2></Link>
+                <Link to="/boardgames"><h2>BOARDGAMES</h2></Link>
                 </div>
-                
-                <img src={magicbox} alt="magicbox" height="200" width="200" id="cards"/>  
+                <div className="links-images">
+                <Link to="/collections"><img src={magicbox} alt="magicbox" height="200" width="200" id="cards"/> </Link>
+                <Link to="/collections"><img src={supplies} alt="card supplies" height="200" width="200" id="supplies"/></Link>
+                <Link to="/boardgames"><img src={boardgame}alt="boardgames" height="200" width="200" id="games"/></Link>
         
-
-                <img src={supplies} alt="card supplies" height="200" width="200" id="supplies"/>    
-        
-                <img src={boardgame}alt="boardgames" height="200" width="200" id="games"/>    
-                
+                </div>
             </div>
         </div>
     )
